fe8a4459d2a4ee10d62029ae041cad29a7f1b574
amityadav288 <cyberwebsite.info@gmail.com>
2026-06-21 10:24:24 +0530

    Update script.js

--- NIDHI MAM HINDI/script.js
+++ NIDHI MAM HINDI/script.js
@@ -30,26 +30,11 @@ function bclick() {
         a.innerHTML = `
         
         
-        <div class="card " > <h2>Class 01</h2><p> सामान्य हिंदी </p><img src="Multimedia/class 1.png" alt="error"> <button> <a href="https://mega.nz/file/jTI30ZjB#nUDxt4M994gewgTkbD18MvEEOgRNaZGXfpKoMfftHSc" target="_blank" >Play List</a></button> </div>    
+        <div class="card " > <h2>Class 01</h2><p> सामान्य हिंदी </p><img src="Multimedia/class 1.png" alt="error"> <button> <a href="https://mega.nz/file/jTI30ZjB#nUDxt4M994gewgTkbD18MvEEOgRNaZGXfpKoMfftHSc" target="_blank" >Play Class</a></button> </div>    
         
-        <div class="card " > <h2>Class 01</h2><p> सामान्य हिंदी </p><img src="Multimedia/class 1.png" alt="error"> <button> <a href="https://mega.nz/file/jTI30ZjB#nUDxt4M994gewgTkbD18MvEEOgRNaZGXfpKoMfftHSc" target="_blank" >Play List</a></button> </div>    
-        
-        <div class="card " > <h2>Class 01</h2><p> सामान्य हिंदी </p><img src="Multimedia/class 1.png" alt="error"> <button> <a href="https://mega.nz/file/jTI30ZjB#nUDxt4M994gewgTkbD18MvEEOgRNaZGXfpKoMfftHSc" target="_blank" >Play List</a></button> </div>    
-        
-        <div class="card " > <h2>Class 01</h2><p> सामान्य हिंदी </p><img src="Multimedia/class 1.png" alt="error"> <button> <a href="https://mega.nz/file/jTI30ZjB#nUDxt4M994gewgTkbD18MvEEOgRNaZGXfpKoMfftHSc" target="_blank" >Play List</a></button> </div>    
-        
-        <div class="card " > <h2>Class 01</h2><p> सामान्य हिंदी </p><img src="Multimedia/class 1.png" alt="error"> <button> <a href="https://mega.nz/file/jTI30ZjB#nUDxt4M994gewgTkbD18MvEEOgRNaZGXfpKoMfftHSc" target="_blank" >Play List</a></button> </div>    
-        
-        <div class="card " > <h2>Class 01</h2><p> सामान्य हिंदी </p><img src="Multimedia/class 1.png" alt="error"> <button> <a href="https://mega.nz/file/jTI30ZjB#nUDxt4M994gewgTkbD18MvEEOgRNaZGXfpKoMfftHSc" target="_blank" >Play List</a></button> </div>    
-        
-        <div class="card " > <h2>Class 01</h2><p> सामान्य हिंदी </p><img src="Multimedia/class 1.png" alt="error"> <button> <a href="https://mega.nz/file/jTI30ZjB#nUDxt4M994gewgTkbD18MvEEOgRNaZGXfpKoMfftHSc" target="_blank" >Play List</a></button> </div>    
-        
-        <div class="card " > <h2>Class 01</h2><p> सामान्य हिंदी </p><img src="Multimedia/class 1.png" alt="error"> <button> <a href="https://mega.nz/file/jTI30ZjB#nUDxt4M994gewgTkbD18MvEEOgRNaZGXfpKoMfftHSc" target="_blank" >Play List</a></button> </div>    
-        
-        <div class="card " > <h2>Class 01</h2><p> सामान्य हिंदी </p><img src="Multimedia/class 1.png" alt="error"> <button> <a href="https://mega.nz/file/jTI30ZjB#nUDxt4M994gewgTkbD18MvEEOgRNaZGXfpKoMfftHSc" target="_blank" >Play List</a></button> </div>    
-        
-        <div class="card " > <h2>Class 01</h2><p> सामान्य हिंदी </p><img src="Multimedia/class 1.png" alt="error"> <button> <a href="https://mega.nz/file/jTI30ZjB#nUDxt4M994gewgTkbD18MvEEOgRNaZGXfpKoMfftHSc" target="_blank" >Play List</a></button> </div>    
-        
+        <div class="card " > <h2>Class 02</h2><p> क्लास 2 वर्णमाला 1</p><img src="Multimedia/class 2.png" alt="error"> <button> <a href="https://mega.nz/file/vT532KwQ#7kHrFPFDhiQ7vlsquJk-EkGAlknMnWg2ywNvvgWt4U8" target="_blank" >Play Cass</a></button> </div>    
+ 
+       
         `
         a.classList.remove('fade-out')
     }, 500)
